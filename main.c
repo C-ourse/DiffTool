a273169ecb9585d43378159ad6bd6c3eb273939a
DimoDimov99 <dimoxd69@gmail.com>
2022-06-13 23:25:43 +0300

    Add compare()
    Found realloc bug in readFile()?

--- main.c
+++ main.c
@@ -21,13 +21,41 @@ int readFile(const char *fileName, struct FileContents *outContents)
     while (!feof(file))
     {
         outContents->nrOfLines++;
-        outContents->lines = realloc(outContents->lines, outContents->nrOfLines);
+        outContents->lines = realloc(outContents->lines, 
+                                     outContents->nrOfLines * sizeof(char *));
 
         size_t dummySize = 0;
         getline(&outContents->lines[outContents->nrOfLines - 1], &dummySize, file);
     }
 
     return EXIT_SUCCESS;
+}
+
+
+int compare(struct FileContents *left, struct FileContents *right)
+{
+    int differences = 0;
+    size_t smaller = left->nrOfLines;
+
+    if (left->nrOfLines > right->nrOfLines)
+    {
+        smaller = right->nrOfLines;
+        differences += left->nrOfLines - right->nrOfLines;
+    }
+    else
+    {
+        differences += right->nrOfLines - left->nrOfLines;
+    }
+    
+    for (size_t i = 0; i < smaller; i++)
+    {
+        if (strcmp(left->lines[i], right->lines[i]) != 0)
+        {
+            differences++;
+        }
+    }
+
+    return differences;
 }
 
 int main(int argc, char *argv[])
@@ -42,17 +70,15 @@ int main(int argc, char *argv[])
     struct FileContents left = {0};
     struct FileContents right = {0};
 
-    if (readFile(argv[1], &left) || readFile(argv[2], &right) != 0)
+    if (readFile(argv[1], &left) || readFile(argv[2], &right))
     {
         exit(EXIT_FAILURE);
     }
     
-
-    printf("%s%s", left.lines[0], left.lines[1]);
-
-    // differences = compare(contents);
-
+    int differences = compare(&left, &right);
+    
     // displayDifferences(differences);
+    printf("Diff lines: %d\n", differences);
 
     return 0;
 }
